375a76826a9d3717fb81f4b354007cb3698bd214
sina <s.malakouti2015@aut.ac.ir>
2016-06-24 14:24:55 +0430

    implementig some parts of humans and humanManager

--- src/thrumania/board/item/GameItems/buildings/Castle.java
+++ src/thrumania/board/item/GameItems/buildings/Castle.java
@@ -1,7 +1,15 @@
 package thrumania.board.item.GameItems.buildings;
 
+import thrumania.board.item.InsideElementsItems;
+
 /**
  * Created by sina on 6/24/16.
  */
-public class Castle {
+public class Castle extends InsideElementsItems{
+    private  String pictureName = "castle.tiff";
+
+
+    public String getPictureName() {
+        return pictureName;
+    }
 }
