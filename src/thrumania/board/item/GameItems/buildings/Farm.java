a2d86389924e5a5c39267165facd8b7d38061c13
sina <s.malakouti2015@aut.ac.ir>
2016-07-03 20:23:30 +0430

    ...

---
+++ src/thrumania/board/item/GameItems/buildings/Farm.java
@@ -0,0 +1,20 @@
+package thrumania.board.item.GameItems.buildings;
+
+import thrumania.board.item.GameItems.LiveElements;
+
+import java.awt.*;
+
+/**
+ * Created by sina on 7/3/16.
+ */
+public class Farm extends LiveElements {
+    @Override
+    public void constructed() {
+
+    }
+
+    @Override
+    public void paintingOptions(Graphics g) {
+
+    }
+}
